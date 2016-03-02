7dfc066dae51818cbc69346d2d849b86707c3c6a
Student <student@d3c1348d24.(none)>
2016-03-02 13:15:18 -0800

    Finished somethings

--- src/edu/balboa/apcs/MineSweeper/ControlPanel.java
+++ src/edu/balboa/apcs/MineSweeper/ControlPanel.java
@@ -3,17 +3,34 @@ package edu.balboa.apcs.MineSweeper;
 import java.util.Scanner;
 
 public class ControlPanel {
-	
+
+	private static int difficulty = 0;
 	int firstInt;
 
-	public ControlPanel() {
-		Scanner sc = new Scanner(System.in);
-		
-		firstInt = new Integer(sc.nextLine());
-		
-	
-		
+	public void diff() {
+
+		while (difficulty == 0) {
+			Scanner sc = new Scanner(System.in);
+			firstInt = new Integer(sc.nextLine());
+
+			if (firstInt == 1) {
+				System.out.println("You chose level easy");
+				difficulty = 8;
+			}
+
+			else if (firstInt == 2) {
+				System.out.println("You chose level medium");
+				difficulty = 12;
+			} else if (firstInt == 3) {
+				System.out.println("You chose level hard");
+				difficulty = 15;
+
+			} else {
+				System.out.println("Invalid Choice");
+
+			}
+		}
+
 	}
-	
-	
+
 }
